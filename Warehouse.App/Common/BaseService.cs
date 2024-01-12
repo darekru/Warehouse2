9c11bb6277b73cb7a21e4068d7d3b3f62d011ff2
druszkiewicz <dariusz.ruszkiewicz@bader-leather.com>
2024-01-12 08:44:42 +0100

    refaktor 11:17

--- Warehouse.App/Common/BaseService.cs
+++ Warehouse.App/Common/BaseService.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Warehouse.App.Abstract;
+using Warehouse.Domain.Common;
 
 namespace Warehouse.App.Common
 {
-    public class BaseService<T> : IService<T>
+    public class BaseService<T> : IService<T> where T : BaseEntity
+
 
 
     {
-        public List<T> Items { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<T> Items { get; set; }
+        public BaseService() 
+        {
+            Items = new List<T>();  
+        }
 
         public int AddItem(T item)
         {
-            throw new NotImplementedException();
+            Items.Add(item);
+            return item.Id;
         }
 
         public List<T> GetAllItems()
